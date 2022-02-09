c74f6cba6059b4b67981c3902e1acaea4ed072b8
Masaki Ito <ito.masaki@em.ci.ritsumei.ac.jp>
2022-02-10 00:19:07 +0900

    re-arrenge files

--- speech_recognition_pkg/setup.py
+++ speech_recognition_pkg/setup.py
@@ -1,8 +1,6 @@
-import os
-from glob import glob
 from setuptools import setup
 
-package_name = 'voice_pkg'
+package_name = 'speech_recognition_pkg'
 
 setup(
     name=package_name,
@@ -12,20 +10,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ito-masaki',
-    maintainer_email='ito.masaki@em.ci.ritsumei.ac.jp',
+    maintainer='root',
+    maintainer_email='root@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'select_answer = voice_pkg.select_answer:main',
-            'speech_recognition = voice_pkg.speech_recognition:main',
-            'speech_synthesis = voice_pkg.speech_synthesis:main',
+            'speech_recognition = speech_recognition_pkg.speech_recognition:main'
         ],
     },
 )
