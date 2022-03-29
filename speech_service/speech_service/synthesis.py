55c8a542ba818c2b408db78a6b872fbb1329f516
Masaki Ito <ito.masaki@em.ci.ritsumei.ac.jp>
2022-03-29 18:32:23 +0900

    [Modify] changed topic communication from service communication

--- speech_service/speech_service/synthesis.py
+++ speech_service/speech_service/synthesis.py
@@ -1,51 +1,49 @@
 import rclpy
 import rclpy.node
-from ai_robot_book_interfaces.srv import StringCommand
+from std_msgs.msg import String
 
 from gtts import gTTS
 from io import BytesIO
 from mpg123 import Mpg123, Out123
 
 
-class SpeechSynthesis(rclpy.node.Node):
+class Synthesis(rclpy.node.Node):
     def __init__(self):
-        super().__init__("speech_synthesis")    
-    
-        self.logger = self.get_logger()        
+        super().__init__("speech_synthesis")
+
+        self.logger = self.get_logger()
         self.logger.info("音声合成ノードを開始します")
 
-        self.lang = 'ja-JP'    
-        self.mp3 = Mpg123()    
-        # self.out = Out123()
+        self.lang = 'ja-JP'
+        self.mp3 = Mpg123()
+        self.out = Out123()
     
-        self.service = self.create_service(StringCommand, '/speech/synthesis', self.synthesis)
-            
-    def synthesis(self, request, response):
+        self.subscriber = self.create_subscription(String, '/speech', self.synthesis, 1)
+
+    def synthesis(self, msg):
         self.get_logger().info('音声合成')
 
-        if not len(request.command):
-            response.answer = 'failed'
-            return response
+        text = msg.data
+        self.get_logger().info(f'\"{text}\"と発話します')
 
-        tts = gTTS(request.command, lang=self.lang[:2])    
-        fp = BytesIO()    
-        tts.write_to_fp(fp)    
-        fp.seek(0)    
-        self.mp3.feed(fp.read())    
-        for frame in self.mp3.iter_frames(self.out.start):    
+        tts = gTTS(text, lang=self.lang[:2])
+        fp = BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+        self.mp3.feed(fp.read())
+
+        for frame in self.mp3.iter_frames(self.out.start):
             self.out.play(frame)
 
-        response.answer = 'succeeded'
-        return response
         
-def main():            
-    rclpy.init()            
-            
-    speech_synthesis = SpeechSynthesis()    
-        
-    try:    
-        rclpy.spin(speech_synthesis)    
-    except:    
-        speech_synthesis.destroy_node()    
-    
+def main():
+    rclpy.init()
+
+    synthesis_node = Synthesis()
+
+    try:
+        rclpy.spin(synthesis_node)
+    except:
+        synthesis_node.destroy_node()
+
     rclpy.shutdown()
